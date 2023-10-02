342d675aeca56a1282cca3566e1971b525e7cdf3
XD <feeelliix75@outlook.com>
2023-10-02 20:33:31 +0200

    ohlyyy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ sys.path.append(r'Z:\INI-Mod\INI-Mod\gui')
 print(sys.path)  # Add this line to print out sys.path for debugging
 from tkinter import ttk
 from ttkthemes import ThemedTk
-from ini_editor import INIEditor  # This import should work if the path is correct
+from gui.ini_editor import INIEditor  # This import should work if the path is correct
 
 def main():
     root = ThemedTk(theme="breeze")  # Set the theme to "breeze"
